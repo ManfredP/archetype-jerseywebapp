2a6f02b2a40be9038f0d5150952975c28d6b5322
Manfred Pusch <manfred@pusch.cc>
2018-09-02 22:08:36 +0200

    first working version

---
+++ src/main/resources/archetype-resources/src/main/java/RestApplicationPath.java
@@ -0,0 +1,8 @@
+package $package;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/webapi")
+public class RestApplicationPath extends Application {
+}
